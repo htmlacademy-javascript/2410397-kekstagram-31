eb5e16d7f916f30e9a3f791df4cb96a8cf237f15
Natalia Melnik <melnik.tasha@yandex.ru>
2024-03-04 16:28:59 +0400

    Restore disappeared code

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,32 @@
+
 // функция для проверки длины строки
 
 const checkStringLength = (string = '', length = 1) => string.length <= length;
 
-checkStringLength();
+checkStringLength('Меня зовут Наташа', 20);
 
-// функция для проверки строки на палиндромность
+//функция для проверки строки на палиндромность
 
 const isPalindrome = (string) => {
-  const normalizedString = string.replaceAll(' ', '').toUpperCase();
-  const reversedString = normalizedString.split('').reverse().join('');
-  return reversedString === normalizedString;
+    const normalizedString = string.replaceAll(' ', '').toUpperCase();
+    const reversedString = normalizedString.split('').reverse().join('');
+    return reversedString === normalizedString;
 };
 
 isPalindrome('Топот');
 
+//функция для извлечения числа
 
+const extractNumbers = (string) => {
+    let result = '';
+    string = string.toString();
+
+    for (let i = 0; i <= string.length - 1; i++) {
+        if (Number.isNaN(parseInt(string[i], 10)) === false) {
+            result += string[i];
+        }
+    }
+    return result === '' ? NaN : Number(result);
+};
+
+extractNumbers('2021 год');
