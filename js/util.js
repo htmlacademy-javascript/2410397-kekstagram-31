158e29e15c8156bb642d1751550c0929d9ecd99c
Natalia-MA <Natalia-MA@users.noreply.github.com>
2024-03-13 06:32:33 +0400

    Create modules

---
+++ js/util.js
@@ -0,0 +1,19 @@
+// функция получения рандомного числа
+
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+// функция получения случайного элемента массива
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+// {
+//   return array[getRandomInteger(0, array.length - 1)]
+// };
+
+export { getRandomArrayElement, getRandomInteger };
